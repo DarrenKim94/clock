32ec1d45f09cdee5bf88f2e52640aeef7f626e94
Darren Kim <dkim555@yahoo.com>
2023-01-31 10:04:27 -0800

    code seconds change

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-const hour = document.getElementById('hour');
-const min = document.getElementById('min');
-const sec = document.getElementById('sec');
+const hourText = document.getElementById('hour');
+const minText = document.getElementById('min');
+const secText = document.getElementById('sec');
+let secCount = 0;
+let changeSec = setInterval(secChange, 1000)
+let minCount = 0;
+let changeMin = setInterval(MinChange, 60000)
+
+function secChange() {
+    secCount += 1;
+    secText.innerHTML = secCount;
+    if (secCount === 60) {
+        clearInterval(changeSec);
+        secCount= 0;
+        setInterval(secChange, 1000);
+    }
+}
+function minChange() {
+    minCount += 1;
+    minText.innerHTML = minCount;
+    if (minCount === 60) {
+        clearInterval(changeMin);
+        minCount= 0;
+        setInterval(minChange, 1000);
+    }
+}
+
+// function secChange() {
+//     secCount += 1;
+//     secText.innerHTML = secCount;
+//     if (secCount === 60) {
+//         clearInterval(changeSec);
+//         secCount= 0;
+//         setInterval(secChange, 1000);
+//     }
+// }
+
+
