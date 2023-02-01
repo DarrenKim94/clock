2c4a80f4f17feeb4bd226b15822007d161157153
Darren Kim <dkim555@yahoo.com>
2023-02-01 14:17:34 -0800

    second method to making a clock

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
-const hourText = document.getElementById('hour');
-const minText = document.getElementById('min');
-const secText = document.getElementById('sec');
-let secCount = 45;
-let timer = setInterval(timeChange, 1000);
-let minCount = 59;
-let hourCount = 11;
+function showTime() {
+    const d = new Date();
+    let hour = d.getHours();
+    let min = d.getMinutes();
+    let sec = d.getSeconds(); 
+    let ampm = 'AM';
+    
+    if (hour > 12) {
+        ampm = 'PM';
+        hour = hour - 12;
+    } 
 
-function timeChange() {
-    secCount += 1;
-    secText.innerHTML = secCount;
-    if (secCount === 60) {
-        secCount = 0;
-        minCount += 1;
-        minText.innerHTML = minCount;
+    if (hour < 10) {
+        hour = `0${hour}`
     }
-    if (secCount < 10) {
-        secText.innerHTML = `:0${secCount}`
-    } else{
-        secText.innerHTML = `: ${secCount}`
+    if (min < 10) {
+        min = `0${min}`
     }
+    if (sec < 10) {
+        sec = `0${sec}`
+    }
+     
+    document.getElementById('clock').innerHTML = `${hour}: ${min}: ${sec} ${ampm}`;
 
-    if (minCount === 60) {
-        hourCount += 1;
-        minCount = 0;
-        hourText.innerHTML = hourCount;
-    }
-
-    if (minCount < 10) {
-        minText.innerHTML = `0${minCount}`
-    }
-
-    if (hourCount > 12) {
-        hourCount = 1;
-        hourText.innerHTML = hourCount;
-    } if (hourCount < 10) {
-        hourText.innerHTML = `0 ${hourCount}:`
-    } else{
-        hourText.innerHTML = `${hourCount} :`
-    }
 }
 
 
+setInterval (showTime, 1000)
